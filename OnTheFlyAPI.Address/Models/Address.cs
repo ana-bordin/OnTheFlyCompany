1928eb9d065acfd117c1843fc20c50f6ab9d4e4b
Ana-bordin <anabordin2008@gmail.com>
2024-06-17 14:53:52 -0300

    models

---
+++ OnTheFlyAPI.Address/Models/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace OnTheFlyAPI.Address.Models
+{
+    public class Address
+    {
+        public string ZipCode { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+        public string Complement { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
--- OnTheFlyAPI.Company/Models/Company.cs
+++ OnTheFlyAPI.Company/Models/Company.cs
@@ -1,7 +1,16 @@
-﻿namespace OnTheFlyAPI.Company.Models
+﻿using OnTheFlyAPI.Address.Models;
+
+namespace OnTheFlyAPI.Company.Models
 {
     public class Company
     {
+        public string Cnpj { get; set; }
+        public string Name { get; set; }
+        public string NameOpt { get; set; }
+        public DateOnly DtOpen { get; set; }
+        public bool Restricted { get; set; }
+        public OnTheFlyAPI.Address.Models.Address Address { get; set; }
 
+        
     }
 }
